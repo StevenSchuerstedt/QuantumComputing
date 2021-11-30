d4ffe820d6ce2571a7f3da265ba082fc8fd4ceb7
Jonas Klamroth <klamroth@fzi.de>
2021-11-30 16:13:31 +0100

    updates orienteering problem, working version

--- orienteering.py
+++ orienteering.py
@@ -2,51 +2,30 @@ import networkx as nx
 import numpy as np
 from docplex.mp.model import Model
 import matplotlib.pyplot as plt
-from qiskit.optimization.applications.ising.common import sample_most_likely
+import matplotlib
+matplotlib.use('tkagg')
 from qiskit_optimization.translators import from_docplex_mp
 from qiskit_optimization.converters import QuadraticProgramToQubo
-from qiskit.algorithms import VQE, NumPyMinimumEigensolver
-import itertools
-from typing import Dict, List, Optional, Union
-from qiskit_optimization.algorithms import OptimizationResult
+from qiskit.algorithms import NumPyMinimumEigensolver
+from typing import Dict, List
 
 
-# def interpret(
-#         result: Union[OptimizationResult, np.ndarray]) -> List[Union[int, List[int]]]:
-#     x = Union[OptimizationResult, np.ndarray]
-#     if isinstance(result, OptimizationResult):
-#         x = result.x
-#     elif isinstance(result, np.ndarray):
-#         x = result
-#     else:
-#         raise TypeError(
-#             "Unsupported format of result. Provide an　OptimizationResult or a",
-#             "binary array using np.ndarray instead of {}".format(type(result)),
-#         )
-#
-#     n = int(np.sqrt(len(x)))
-#     route = []  # type: List[Union[int, List[int]]]
-#     for p__ in range(n):
-#         p_step = []
-#         for i in range(n):
-#             if x[i * n + p__]:
-#                 p_step.append(i)
-#         if len(p_step) == 1:
-#             route.extend(p_step)
-#         else:
-#             route.append(p_step)
-#     return route
+### some options
+showGraph = False
+solveClassically = True
+solveQuantumlike = True
 
-def draw_graph(G):
-    labels = {n: str(n) + ';   ' + str(G.nodes[n]['weight']) for n in G.nodes}
+
+def drawGraph(G):
+    labels = {n: str(n) + ': ' + str(G.nodes[n]['weight']) for n in G.nodes}
     colors = [G.nodes[n]['weight'] for n in G.nodes]
     pos = nx.spring_layout(G)
-    nx.draw(G, with_labels=True, labels=labels, node_color=colors)
+    nx.draw(G, with_labels=True, labels=labels, node_color=colors, node_size=1000)
     edge_labels = nx.get_edge_attributes(G, 'weight')
     nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
     plt.show()
 
-def to_integer_program(G):
+def to_integer_program(G, solveClassically):
     mdl = Model(name="Orienteering")
     n = G.number_of_nodes()
     x = {
@@ -54,84 +33,158 @@ def to_integer_program(G):
         for i in range(n)
         for j in range(n)
     }
+
+    u = {
+        i : mdl.integer_var(name="u_" + str(i), lb=2, ub=n)
+        for i in range(1, n)
+    }
+
     orienteering_func = mdl.sum(
-        G.nodes[i]["weight"] * x[(i, j)]
+        G.nodes[j]["weight"] * x[(i, j)]
         for i in range(n)
         for j in range(n)
     )
     mdl.maximize(orienteering_func)
 
-    # Only 1 edge goes out from each node
-    for i in range(n):
-        mdl.add_constraint(mdl.sum(x[(i, j)] for j in range(n) if i != j) == 1)
+    for i in range(1, n):
+        for j in range(1, n):
+            mdl.add_constraint(u[i] - u[j] + 1 <= (n - 1)*(1 - x[(i, j)]))
 
-    # Only 1 edge comes into each node
-    for j in range(n):
-        mdl.add_constraint(mdl.sum(x[(i, j)] for i in range(n) if i != j) == 1)
+    for k in range(1, n):
+        # a node has to have same number of in and out going edges
+        # => there are no dead ends
+        mdl.add_constraint(mdl.sum(x[i, k] for i in range(n)) == mdl.sum(x[k, j] for j in range(n)))
+        # a node may appear only once 
+        mdl.add_constraint(mdl.sum(x[k, j] for j in range(n)) <= 1)
 
-    # flow conservation conditions?
-    # sackgassen vermeiden?
-    for k in range(n):
-        mdl.add_constraint(mdl.sum(x[(i, k)] for i in range(n)) <= 1)
+    
+    # we start and end at node 0
+    # at least together with the above constraints this is what this is for
+    mdl.add_constraint(mdl.sum(x[0, j] for j in range(1, n)) == 1)
+    mdl.add_constraint(mdl.sum(x[j, 0] for j in range(1, n)) == 1)
 
-    for k in range(n):
-        mdl.add_constraint(mdl.sum(x[(k, i)] for i in range(n)) <= 1)
-
+    # do not exceed threshold
     mdl.add_constraint(mdl.sum(
                 G.edges[i, j]["weight"] * x[(i, j)]
                 for i in range(n)
                 for j in range(n)
-                if i != j
             ) <= T_max)
 
-    # To eliminate sub-routes
-    node_list = [i for i in range(n)]
-    clique_set = []
-    for i in range(2, len(node_list) + 1):
-        for comb in itertools.combinations(node_list, i):
-            clique_set.append(list(comb))
-    for clique in clique_set:
-        mdl.add_constraint(
-            mdl.sum(x[(i, j)] for i in clique for j in clique if i != j) <= len(clique) - 1
-        )
+    mdl.prettyprint()
+    if solveClassically:
+        print("Classical solution:")
+        mdl.solve()
+        print("Best route cost: " + str(mdl.blended_objective_values[0]))
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                var = mdl.get_var_by_name(x[i, j].lp_name).raw_solution_value
+                if var > 0:
+                    solution.append(str(x[i, j]))
 
+    print("Best route: " + str(solution))
     op = from_docplex_mp(mdl)
     return op
 
-
-# create a simple orienteering graph
-G = nx.Graph()
-G.add_node(0, weight=1)
-G.add_node(1, weight=1)
-G.add_node(2, weight=1)
-#G.add_node(3, weight=2)
-G.add_edge(0, 1, weight=10 )
-G.add_edge(0, 2, weight=10 )
-#G.add_edge(0, 3, weight=2 )
-G.add_edge(1, 2, weight=10 )
-#G.add_edge(1, 3, weight=5 )
-#G.add_edge(2, 3, weight=7 )
-T_max = 20
-#draw_graph(G)
+def interpret(res, qp):
+    bits = []
+    for i, c in enumerate(bin(res)[:1:-1], 1):
+        if c == '1':
+            bits.append(i - 1)
+    
+    vars = [qp.variables[i].name for i in range(len(qp.variables)) if i in bits]
+    vars = list(filter(lambda x: x[0] != 'u', vars))
+    vars.sort()
+    return vars
 
 
-###convert tsp to quadratic program
-qp = to_integer_program(G)
-qp2qubo = QuadraticProgramToQubo()
-qubo = qp2qubo.convert(qp)
+def getExampleGraph1():
+    # create a simple orienteering graph (directed)
+    G = nx.DiGraph()
+    G.add_node(0, weight=2)
+    G.add_node(1, weight=8)
+    G.add_node(2, weight=6)
+    G.add_node(3, weight=2)
+    G.add_edge(0, 1, weight=5 )
+    G.add_edge(1, 0, weight=5 )
+    G.add_edge(2, 3, weight=5)
+    G.add_edge(3, 2, weight=5)
+    return G
 
-###convert to ising hamiltonian
-qubitOp, offset = qubo.to_ising()
+def getExampleGraph():
+    # create a simple orienteering graph (directed)
+    G = nx.DiGraph()
+    G.add_node(0, weight=2)
+    G.add_node(1, weight=8)
+    G.add_node(2, weight=6)
+    G.add_node(3, weight=2)
+    G.add_edge(0, 1, weight=10 )
+    G.add_edge(1, 0, weight=20 )
+    G.add_edge(1, 2, weight=5)
+    G.add_edge(2, 3, weight=5)
+    G.add_edge(3, 0, weight=5)
+    G.add_edge(2, 0, weight=5)
+    G.add_edge(0, 2, weight=10)
+    return G
 
-#solve using numpy
+def getSmallExmapleGraph1():
+    G = nx.DiGraph()
+    G.add_node(0, weight=2)
+    G.add_node(1, weight=8)
+    G.add_node(2, weight=6)
+    G.add_edge(0, 1, weight=10 )
+    G.add_edge(1, 0, weight=10 )
+    G.add_edge(1, 2, weight=25)
+    G.add_edge(2, 0, weight=5)
+    return G
 
-ee = NumPyMinimumEigensolver()
-result = ee.compute_minimum_eigenvalue(qubitOp)
+def getSmallExmapleGraph():
+    G = nx.DiGraph()
+    G.add_node(0, weight=2)
+    G.add_node(1, weight=8)
+    G.add_node(2, weight=6)
+    G.add_edge(0, 1, weight=10 )
+    G.add_edge(1, 0, weight=20 )
+    G.add_edge(1, 2, weight=5)
+    G.add_edge(2, 0, weight=5)
+    return G
 
-print('energy:', result.eigenvalue.real)
-print('tsp objective:', result.eigenvalue.real + offset)
-#x = sample_most_likely(result.eigenstate)
-#print('feasible:', qubo.is_feasible(x))
-#z = interpret(x)
-#print('solution:', z)
-# print('solution objective:', tsp.tsp_value(z, adj_matrix))
+
+def solveWithQC(qp):
+    #convert integer program to qubo
+    qp2qubo = QuadraticProgramToQubo()
+    qubo = qp2qubo.convert(qp)
+
+    #convert to ising hamiltonian
+    qubitOp, offset = qubo.to_ising()
+
+    #solve using numpy
+    ee = NumPyMinimumEigensolver()
+    result = ee.compute_minimum_eigenvalue(qubitOp)
+
+    #print('energy:', result.eigenvalue.real)
+    print("Quantum solution:")
+    print('Best route cost:', (result.eigenvalue.real + offset) * -1)
+    x = np.argmax(result.eigenstate.primitive)
+    interpretation = interpret(x, qp)
+    print('Best route:', interpretation)
+
+def makeFullyConnectedGraph(G):
+    for (i, j) in [(i, j) for i in range(len(G.nodes)) for j in range(len(G.nodes))]:
+        if (i, j) not in G.edges:
+            G.add_edge(i, j, weight=T_max * 2)
+
+T_max = 30
+G = getSmallExmapleGraph()
+makeFullyConnectedGraph(G)
+
+if showGraph:
+    drawGraph(G)
+    
+#encode problem as constraint integer program
+qp = to_integer_program(G, solveClassically)
+
+if solveQuantumlike:
+    solveWithQC(qp)
+
+
